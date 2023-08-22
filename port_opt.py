21ff594ae3e379fbaeb06652c851777ec61317f3
Nav-Hira <71988836+Nav-Hira@users.noreply.github.com>
2023-08-22 16:03:36 +0100

    Update port_opt.py

--- port_opt.py
+++ port_opt.py
@@ -60,7 +60,7 @@ print (adj_close_df.head(5))
 
 import plotly.express as px
 df = px.data.stocks()
-fig = px.line(adj_close_df, x="Date", y=adj_close_df.columns,
+fig = px.line(adj_close_df, x=adj_close_df.index, y=adj_close_df.columns,
               hover_data={"date": "|%B %d, %Y"},
               title='custom tick labels')
 fig.update_xaxes(
